2f61bbfa07ac42d1a66038fc89f546046ba3b648
Nikhil Kansal <nkansal96@gmail.com>
2017-12-07 21:10:33 -0800

    better error handling

--- auroraapi/api.py
+++ auroraapi/api.py
@@ -1,4 +1,4 @@
-import audio
+import audio, pprint
 import requests, functools
 
 BASE_URL = "https://api.auroraapi.com"
@@ -16,7 +16,9 @@ def get_tts(app_id, app_token, text):
 
 	r = requests.get(TTS_URL, params=[("text", text)], headers=headers, stream=True)
 	if r.status_code != requests.codes.ok:
-		raise APIException(r.json()["message"])
+		if "application/json" in r.headers["content-type"]:
+			raise APIException(r.json()["message"])
+		raise APIException(r.text)
 
 	r.raw.read = functools.partial(r.raw.read, decode_content=True)
 	return audio.AudioFile.create_from_http_stream(r.raw)
@@ -32,6 +34,10 @@ def get_stt(app_id, app_token, audio):
 	}
 
 	r = requests.post(STT_URL, headers=headers, files=files)
-	if r.status_code != requests.codes.ok or r.json().has_key("message"):
-		raise APIException(r.json()["message"])
+	if r.status_code != requests.codes.ok:
+		if "application/json" in r.headers["content-type"]:
+			raise APIException(r.json()["message"])
+		if r.status_code == 413:
+			raise APIException("Request entity too large")
+		raise APIException(r.text)
 	return r.json()
