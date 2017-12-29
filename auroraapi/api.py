c8d93cd11c34a48ff329e9981ddb40a8819e7217
Nikhil Kansal <nkansal96@gmail.com>
2017-12-29 15:55:09 -0800

    improve error handling

--- auroraapi/api.py
+++ auroraapi/api.py
@@ -8,6 +8,13 @@ INTERPRET_URL = BASE_URL + "/v1/interpret/"
 
 class APIException(Exception):
 	""" Raise an exception when querying the API """
+	def __init__(self, id, status, code, type, message):
+		self.id = id
+		self.status = status
+		self.code = code
+		self.type = type
+		self.message = message
+		super(APIException, self).__init__("[{}] {}".format(code, message))
 
 def get_headers():
 	return {
@@ -16,30 +23,27 @@ def get_headers():
 		"X-Device-ID": globals.DEVICE_ID,
 	}
 
-def get_tts(text):
-	r = requests.get(TTS_URL, params=[("text", text)], headers=get_headers(), stream=True)
+def handle_error(r):
 	if r.status_code != requests.codes.ok:
 		if "application/json" in r.headers["content-type"]:
-			raise APIException(r.json()["message"])
+			raise APIException(**r.json())
+		if r.status_code == 413:
+			raise APIException(code="RequestEntityTooLarge", message="Request entity too large")
 		raise APIException(r.text)
+
+def get_tts(text):
+	r = requests.get(TTS_URL, params=[("text", text)], headers=get_headers(), stream=True)
+	handle_error(r)
 
 	r.raw.read = functools.partial(r.raw.read, decode_content=True)
 	return audio.AudioFile.create_from_http_stream(r.raw)
 
 def get_interpret(text):
 	r = requests.get(INTERPRET_URL, params=[("text", text)], headers=get_headers())
-	if r.status_code != requests.codes.ok:
-		if "application/json" in r.headers["content-type"]:
-			raise APIException(r.json()["message"])
-		raise APIException(r.text)
+	handle_error(r)
 	return r.json()
 
 def get_stt(audio):
 	r = requests.post(STT_URL, files={ "audio": audio.get_wav() }, headers=get_headers())
-	if r.status_code != requests.codes.ok:
-		if "application/json" in r.headers["content-type"]:
-			raise APIException(r.json()["message"])
-		if r.status_code == 413:
-			raise APIException("Request entity too large")
-		raise APIException(r.text)
+	handle_error(r)
 	return r.json()
