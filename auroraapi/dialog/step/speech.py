cc72a8ad92762b82a94862bd64de043a478f1472
Nikhil Kansal <nkansal96@gmail.com>
2018-06-27 14:47:50 +0530

    fix context path resolution bug, add str support, convert non-str to str for templatized strings

--- auroraapi/dialog/step/speech.py
+++ auroraapi/dialog/step/speech.py
@@ -22,9 +22,12 @@ def resolve_path(context, path):
     # Check if the obj is a dict and does not have the given key in it
     if isinstance(obj, dict) and curr not in obj:
       return None
-    # Check if the object is a list and if the key is an integer
-    if isinstance(obj, list) and parse_optional(curr, int, None) != None:
-      curr = int(curr)
+    # Check if the object is a list or string
+    if isinstance(obj, (list, str)):
+      # If the object is a list, the key must be an integer
+      curr = parse_optional(curr, int, None)
+      if curr == None:
+        return None
       # Check if the object type is a list, the key is an int, but is out of bounds
       if curr >= len(obj):
         return None
@@ -38,18 +41,18 @@ class SpeechStep(Step):
     self.text = step["data"]["text"]
     self.step_name = step["data"]["stepName"]
   
-  def get_text(self):
+  def get_text(self, context):
     # upon execution, first find all templates and replace them with
     # the collected value in the conversation
     replacements = []
     for match in re.finditer(r'(\${(.+?)})', self.text):
       val = resolve_path(context, match.group(2))
       # TODO: do something if val not found
-      replacements.append((match.group(1), val))
+      replacements.append((match.group(1), str(val)))
     return reduce(lambda t, r: t.replace(r[0], r[1]), replacements, self.text)
 
   def execute(self, context, edge):
-    sp = Text(self.get_text()).speech()
+    sp = Text(self.get_text(context)).speech()
     context.set_step(self.step_name, sp)
     sp.audio.play()
     return edge.next()
